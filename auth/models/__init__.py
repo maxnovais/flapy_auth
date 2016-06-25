645ada182a971ef6d62caacc65fe7dcdf822b703
Maxwell <maxnovais@gmail.com>
2016-06-24 22:24:42 -0300

    Added ignore line

--- auth/models/__init__.py
+++ auth/models/__init__.py
@@ -101,6 +101,6 @@ class Model(db.Model, ModelMixin):
     query_class = Query
 
 
-from auth.models.user import User
-from auth.models.role import Role
-from auth.models.user_role import UserRole
+from auth.models.user import User #NSQA
+from auth.models.role import Role #NSQA
+from auth.models.user_role import UserRole #NSQA
