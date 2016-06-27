1322296856066812431fd9497c6d5cad45d08561
Maxwell <maxnovais@gmail.com>
2016-06-27 08:46:52 -0300

    Added Alchemy config in base

--- auth/config/base.py
+++ auth/config/base.py
@@ -4,5 +4,7 @@ import os
 
 class BaseConfig(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+
     SECRET_KEY = os.environ.get('SECRET_KEY')
     DEBUG = False
